f36ef40b4822da4821ae5899795f47f901f11f34
BaptBat <163831165+BaptBat@users.noreply.github.com>
2026-01-30 15:28:07 +0000

    Deploying to gh-pages from @ BaptBat/slam@4eba3699c4303b6397c49bf009acfcf4fb21521b 🚀

---
+++ _downloads/66563413d6aa67c1f801481d818d42ec/example_vertex_voronoi.py
@@ -0,0 +1,69 @@
+"""
+.. _example_vertex_voronoi:
+
+===================================
+Vertex voronoi example in slam
+===================================
+"""
+
+# Authors:
+# Guillaume Auzias <guillaume.auzias@univ-amu.fr>
+# Julien Barrès <julien.barres@etu.univ-amu.fr>
+
+# License: MIT
+# sphinx_gallery_thumbnail_number = 2
+
+###############################################################################
+# NOTE: there is no visualization tool in slam, but we provide at the
+# end of this script exemplare code to do the visualization with
+# an external solution
+###############################################################################
+
+###############################################################################
+# Importation of slam modules
+import slam.io as sio
+import slam.vertex_voronoi as svv
+import numpy as np
+
+
+###############################################################################
+mesh = sio.load_mesh("../examples/data/example_mesh.gii")
+mesh.apply_transform(mesh.principal_inertia_transform)
+
+###############################################################################
+vert_vor = svv.vertex_voronoi(mesh)
+print(mesh.vertices.shape)
+print(vert_vor.shape)
+print(np.sum(vert_vor) - mesh.area)
+
+#############################################################################
+# VISUALIZATION USING EXTERNAL TOOLS
+#############################################################################
+import slam.plot as splt
+###############################################################################
+# Visualization
+
+vertices = mesh.vertices
+vertices = vertices - np.mean(vertices, axis=0)
+
+vertices_translate = np.copy(vertices)
+theta = np.pi / 2
+rot_x = np.array([[1, 0, 0],
+                  [0, np.cos(theta), -np.sin(theta)],
+                  [0, np.sin(theta),  np.cos(theta)]])
+vertices_translate = np.dot(rot_x, vertices_translate.T).T
+
+display_settings = {}
+mesh_data = {}
+mesh_data['vertices'] = vertices_translate
+mesh_data['faces'] = mesh.faces
+mesh_data['title'] = 'example_mesh.gii'
+
+intensity_data = {}
+intensity_data['values'] = vert_vor
+intensity_data["mode"] = "vertex"
+visb_sc = splt.mes3d_projection(
+    mesh_data=mesh_data,
+    intensity_data=intensity_data,
+    display_settings=display_settings)
+visb_sc.show()
---
+++ _downloads/9a51f8a84301b2cf1a752c4ec46aba43/example_texture.py
@@ -0,0 +1,85 @@
+"""
+.. _example_texture:
+
+===================================
+Texture example in slam
+===================================
+"""
+
+# Authors:
+# Guillaume Auzias <guillaume.auzias@univ-amu.fr>
+# Julien Barrès <julien.barres@etu.univ-amu.fr>
+
+# License: MIT
+# sphinx_gallery_thumbnail_number = 2
+
+
+###############################################################################
+# Importation of slam modules
+import os
+from pathlib import Path
+import numpy as np
+from slam import texture
+from slam import io as sio
+from slam import plot as plt
+
+###############################################################################
+#
+tex = sio.load_texture("../examples/data/example_dpf.gii")
+mesh = sio.load_mesh("../examples/data/example_mesh.gii")
+print(tex)
+print(tex.metadata)
+print(tex.shape)
+print(tex.dtype)
+print(tex.min())
+print(tex.max())
+
+
+###############################################################################
+#
+darray = np.zeros((2, 3))
+tex2 = texture.TextureND(darray=darray)
+print(tex2.metadata)
+print(tex2)
+print(tex2.shape)
+print(tex2.dtype)
+print(tex2.min())
+print(tex2.max())
+sio.write_texture(tex2, "test.gii")
+
+#############
+print('extremum texture')
+mesh = sio.load_mesh("../examples/data/example_mesh.gii")
+print('maximum')
+print(np.count_nonzero(tex.extremum(mesh) == 1))
+print('minimum')
+print(np.count_nonzero(tex.extremum(mesh) == -1))
+
+###############################################################################
+# plot
+
+# dict for proj
+NAME_TEX = "sulc"
+TITLE = "test"
+EXT = "png"
+PATH = Path("./test")
+SAVE_DIR = PATH / f"{TITLE}.{EXT}"
+
+mesh_data = {
+    "vertices": mesh.vertices,
+    "faces": mesh.faces,
+    "center": mesh.center_mass,
+    "title": TITLE
+}
+
+intensity_data = {"values": tex.darray[0], "mode": "vertex"}
+display_settings = {"colorscale": "Turbo", "colorbar_label": NAME_TEX}
+
+fig = plt.mes3d_projection(
+    mesh_data,
+    intensity_data,
+    display_settings,
+)
+
+#os.makedirs(PATH, exist_ok=True)
+#fig.write_image(SAVE_DIR, width=1600, height=900)
